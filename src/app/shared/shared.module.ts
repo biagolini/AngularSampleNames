ee0090ad7231974cfe861136b5f8b9572d9ffc1a
Carlos Biagolini-Jr <c.biagolini@gmail.com>
2022-09-16 13:36:10 -0300

    Fix missing dependencies

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,13 +2,16 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 import { MatChipsModule } from '@angular/material/chips';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 import { MatTableModule } from '@angular/material/table';
 import { RouterModule } from '@angular/router';
 
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+
 
 
 var module = [
@@ -20,7 +23,10 @@ var module = [
   ReactiveFormsModule,
   RouterModule,
   MatChipsModule,
+  MatButtonModule,
+  MatInputModule,
 ];
+
 
 @NgModule({
   declarations: [
